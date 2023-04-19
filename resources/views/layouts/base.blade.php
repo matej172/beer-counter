96113fcd14c3ae902ff96640000c893809d2d019
Matej <matej.rabek@gmail.com>
2023-04-20 00:51:38 +0200

    ADD: pwa

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,11 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name') }}</title>
 
-		<link rel="manifest" href="/site.webmanifest">
+        <meta name="description" content="Beer counting application">
+
+        <link rel="icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+        <link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF">
+        <meta name="theme-color" content="#ffffff">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="manifest" href="/build/manifest.webmanifest">
+        <script src="/build/registerSW.js"></script>
     </head>
     <body class="min-h-screen flex flex-col w-full items-center bg-green-900">
         @yield('body')
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -6,6 +7,39 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        VitePWA({
+            devOptions: {
+                enabled: true
+                /* other options */
+            },
+            buildBase: '/build/',
+            injectRegister: 'auto',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'android-chrome-192x192.png'],
+            manifest: {
+                name: 'My Awesome App',
+                short_name: 'MyApp',
+                description: 'My Awesome App description',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
         }),
     ],
 });
